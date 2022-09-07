19ff18e02396cd5ff1d1da753e8ac85784b6ea4a
우예인 <uyeinyein@gmail.com>
2022-09-07 17:49:54 +0900

    중요한 데이터는 변수말고 state에 담습니다

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
+
+  // 변수대신 state를 이용한 데이터 저장
+  let [글제목, b] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [logo, setLogo] = useState("ReactBlog");
+
+  // let [a, c] = [1, 2];
+  // let a = num[0];
+  // let c = num[1];
 
   return (
     <div className="App">
@@ -12,9 +21,20 @@ function App() {
       JSX 문법3. style 넣을땐 style={{스타일명:'값'}}
       */}
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
