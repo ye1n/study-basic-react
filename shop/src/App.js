e3f4c7406e9416eb5a0424f78c63e0c317e7eca2
우예인 <uyeinyein@gmail.com>
2022-09-19 15:48:44 +0900

    이미지 넣는 법 & public 폴더 이용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Col, Container, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#Shop">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      {/* public 폴더 안 이미지 사용: {process.env.PUBLIC_URL + '/이미지 경로'} */}
+      <Container>
+        <Row>
+          <Col>
+            {/* html안에서 이미지 사용 : 이미지경로 import한 후 작명한걸 사용 */}
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
