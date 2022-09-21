bd9ad4d91c19d9d332b2bd7f582a243548d5c0fd
우예인 <uyeinyein@gmail.com>
2022-09-21 11:07:23 +0900

    Lifecycle과 useEffect 1

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
@@ -16,21 +17,47 @@ let YellowBtn = styled.button`
 //     padding : 20px;
 // `
 
+// 컴포넌트의 Lifecycle : 예전 방식
+// 페이지에 장착(mount), 업데이트(update), 필요없다면 제거(unmout)
+
+// class Detail2 extends React.Component {
+//     componentDidMount(){
+
+//     }
+//     componentDidUpdate(){
+
+//     }
+//     componentWillUnmount(){
+
+//     }
+// }
+
 function Detail(props) {
 
     let { id } = useParams();   // 현재 /:url파라미터 자리에 유저가 입력한 값을 가져온다.
 
     // 같은 문법 두가지
-    // let 찾은상품 = props.shoes.find(function (x) {
-    //     return x.id == id
-    // });
+    // let 찾은상품 = props.shoes.find(function (x) { return x.id == id });
     let 찾은상품 = props.shoes.find((x) => x.id == id);
+
+    // 컴포넌트의 useEffect : 요즘 방식
+    // useEffect 안에 있는 코드는 html 렌더링 후에 동작 (어려운 연산, 서버에서 데이터 가져오는 작업, 타이머 사용 코드)
+    useEffect(()=>{
+        // mount, update 시 실행 됨
+        setTimeout(()=>{setAlert(false)}, 2000);
+    })
+
+    let [alert, setAlert] = useState(true);
 
     return (
         <div className="container">
 
             {/* <YellowBtn bg="blue">버튼</YellowBtn>
             <YellowBtn bg="orange">버튼</YellowBtn> */}
+
+            {
+                alert == true ? <div className="alert alert-warning">2초 이내 구매시 할인</div> : null
+            }
 
             <div className="row">
                 <div className="col-md-6">
